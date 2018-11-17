28b58975694f398854f32f71f40483917354f52f
Luis Pavón <luis.pavon.lmpp@gmail.com>
2018-11-16 22:42:36 -0600

    Modificación para cargar el archivo necesario para los calculos

--- Nota_tecnica.py
+++ Nota_tecnica.py
@@ -23,6 +23,19 @@ def Nota_tec():
                     datos[i]={}
             pass
         elif x == "C" or "c":
+                with open("Datos _fianzas_2008.csv", newline='') as f:
+                reader = csv.reader(f)
+                n_row=0
+                datos={}
+                Ramos=[]                
+                for row in reader: 
+                    Ramos.append(row[0])
+                Ramos.pop(0)
+                for row in reader:
+                    for i in row:
+                        Columnas.append(row[i])
+                for i in Ramos:
+                    datos[i]={}
             pass
         else:
             x=0
